bd2895710884750b2d9ec92e264972e99e7b5ea0
ATM332 <Awrad.almatouq@gmail.com>
2022-09-01 05:32:26 +0300

    backup3

--- app/src/main/java/com/example/review/LoginActivity.java
+++ app/src/main/java/com/example/review/LoginActivity.java
@@ -1,20 +1,32 @@
 package com.example.review;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    private TextView signup;
-    private Button login;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView signup,login;
     private EditText edittextemail, edittextpassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +34,86 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signup = findViewById(R.id.signup_TV);
+        login = findViewById(R.id.loginbttn);
+
+        edittextemail= findViewById(R.id.input_email2);
+        edittextpassword= findViewById(R.id.input_pass2);
+
+        progressBar= findViewById(R.id.progressbar);
+
+        mAuth= FirebaseAuth.getInstance();
 
 
-        signup.setOnClickListener(new View.OnClickListener() {
+
+
+        signup.setOnClickListener(this);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.signup_TV:
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                break;
+
+            case R.id.loginbttn:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email= edittextemail.getText().toString();
+        String password= edittextpassword.getText().toString();
+
+        if(email.isEmpty()){
+            edittextemail.setError("Email is required");
+            edittextemail.requestFocus();
+            return;
+
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edittextemail.setError("Please enter valied email");
+            edittextemail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            edittextpassword.setError("Password is required");
+            edittextpassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            edittextpassword.setError("Password must be 6 characters or more!");
+            edittextpassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
-                Intent intentreg = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intentreg);
+                if(task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    if(user.isEmailVerified()) {
+                        //redirect to user profile
+                        startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                    }else{
+                        user.sendEmailVerification();
+                        Toast.makeText(LoginActivity.this, "Check your email to verify your account", Toast.LENGTH_LONG).show();
+                    }
+
+
+                }else{
+                    Toast.makeText(LoginActivity.this, "Failed to Login! Please check your credentails", Toast.LENGTH_LONG).show();
+
+                }
 
             }
         });
+
     }
 }
